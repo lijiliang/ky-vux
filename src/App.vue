3a8adf54f590ce5e6efcc0b4e07aea8ed4d14570
Benson <benson@Benson.local>
2017-11-16 18:34:07 +0800

    feat: 底部bar

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,26 @@
         <div>kyani</div>
       </x-header>
       <router-view></router-view>
-      <tabbar slot="bottom">
-        <tabbar-item>
+      <tabbar slot="bottom" class="m-tabbar">
+        <tabbar-item selected>
+          <i slot="icon" class="icon icon-Kyani"></i>
           <span slot="label">首页</span>
         </tabbar-item>
         <tabbar-item>
-          <span slot="label">首页</span>
+          <i slot="icon" class="icon icon-news"></i>
+          <span slot="label">最新消息</span>
         </tabbar-item>
         <tabbar-item>
-          <span slot="label">首页</span>
+          <i slot="icon" class="icon icon-productlist"></i>
+          <span slot="label">产品商城</span>
+        </tabbar-item>
+         <tabbar-item>
+          <i slot="icon" class="icon icon-shoppingCart"></i>
+          <span slot="label">购物车</span>
         </tabbar-item>
         <tabbar-item>
-          <span slot="label">首页</span>
+          <i slot="icon" class="icon icon-login"></i>
+          <span slot="label">我的</span>
         </tabbar-item>
       </tabbar>
       
@@ -40,6 +48,7 @@ export default {
 
 <style lang="less">
 @import '~vux/src/styles/reset.less';
+@import './base/resources/less/iconfont.less';
 html, body {
     height: 100%;
     width: 100%;
@@ -53,6 +62,13 @@ html, body {
     left:0;
     top:0;
     z-index:100;
+  }
+}
+.m-tabbar{
+  background: #fff!important;
+  i.icon{
+    line-height: 1;
+    font-size: 24px;
   }
 }
 </style>
